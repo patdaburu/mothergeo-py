3b057962976fd318f440e5006f9036410ce16265
patdaburu <pat@daburu.net>
2020-01-24 13:24:16 -0600

    Backed out of change to conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'source/index'
+master_doc = 'index'
 
 # General information about the project.
 project = 'mothergeo'
